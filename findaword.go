5a6a2bd84905c9735bffbbdd05ae00c8b6c638a4
Vladimir <110772568+Vova4o@users.noreply.github.com>
2023-12-03 15:21:19 +0300

    Some changes

---
+++ findaword.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"unicode/utf8"
+)
+
+func readTextFile() []string {
+
+	//Pass the file name to the ReadFile() function from
+	//the util package to get the content of the file.
+
+	content, error := os.ReadFile("russian_nouns.txt")
+
+	// defer .Close()
+	// Check whether the 'error' is nil or not. If it
+	//is not nil, then print the error and exit.
+	if error != nil {
+
+		log.Fatal(error)
+	}
+
+	// convert the content into a string
+	str := string(content)
+
+	arr := strings.Split(str, "\n")
+	//Print the string str
+	return arr
+}
+
+func findMatch(arr []string, letter string) []string {
+
+	newArr := []string{}
+
+	for _, val := range arr {
+		if strings.Contains(val, letter) {
+			newArr = append(newArr, val)
+		}
+	}
+
+	return newArr
+}
+
+func shrinkByLen(arr []string, a int) []string {
+
+	newArr := []string{}
+
+	for _, val := range arr {
+		numVal := utf8.RuneCountInString(val)
+		if numVal <= a {
+			newArr = append(newArr, val)
+		}
+	}
+
+	return newArr
+
+}
+
+// func letterOrNumber(s string)  {
+
+// }
+
+func main() {
+	arr := readTextFile()
+
+	fmt.Println(len(arr))
+
+	endProgram := false
+
+	for !endProgram {
+		var letter string
+		fmt.Println("Если хотите закончить программу, напишите - конец")
+		fmt.Println("Если вы хотите ограничить длину поиска, напишите - число")
+		fmt.Println("Enter letter: ")
+		fmt.Scan("%s\n", &letter)
+		if letter == "конец" {
+			endProgram = true
+			break
+		}
+		if letter == "число" {
+			var a int
+			fmt.Println("Введите число: ")
+			fmt.Scan("%d\n", &a)
+			arr = shrinkByLen(arr, a)
+			fmt.Println(arr)
+			fmt.Println(len(arr))
+		}
+
+		if letter != "число" || letter != "конец" {
+			arr = findMatch(arr, letter)
+			fmt.Println(arr)
+			fmt.Println(len(arr))
+		}
+
+		// Problem that I run in to, is that the program will call
+		// findMatch even then число is entered
+		// that brakes the program...
+		// arr = shrinkByLen(arr, len(letter))
+
+	}
+
+}
